5bd341745e1847163c8bd2eff35c05cad5979cd2
JC Software Solution <engjcsoftwaresolution@gmail.com>
2024-04-05 17:54:54 +0530

    event detail

--- src/Pages/Dashboard/index.tsx
+++ src/Pages/Dashboard/index.tsx
@@ -122,10 +122,10 @@ const Dashboard = () => {
                     <button type="button"
                         onClick={() => history(`/event/detail/${itm.id}`)}
                         className="text-white bg-orange-500 w-[49%] hover:bg-orange-600 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center">View Event</button>
-                    {itm.meetingStatus == 'completed' || filters.type != 'ongoing'? <></> : <>
+                    {/* {itm.meetingStatus == 'completed' || filters.type != 'ongoing'? <></> : <>
                         <button type="button" onClick={() => endEvent(itm.id)}
                             className="text-white bg-gradient-to-r w-[49%] from-red-400 via-red-500 to-red-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center">End Event</button>
-                    </>}
+                    </>} */}
 
 
                 </div>
