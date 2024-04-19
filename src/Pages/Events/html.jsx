ed9811171f84f549d4d58c4f7c02c1b8b1947c81
Frontend RVRenterCenter <frontend@rvrentercenter.com>
2024-04-19 10:44:08 +0500

    chnages for plan page responsive

--- src/Pages/Events/html.jsx
+++ src/Pages/Events/html.jsx
@@ -146,7 +146,7 @@ const Html = ({
 
 
             <div className='shadow-box w-full bg-white rounded-lg mt-6'>
-                <div className='flex p-4'>
+                <div className='flex p-4 items-center flex-wrap'>
 
                     <form class="flex items-center max-w-sm" onSubmit={e => {
                         e.preventDefault()
@@ -175,7 +175,7 @@ const Html = ({
                         ></i>
                       )}
                         </div>
-                        <button type="submit" class="p-2.5 ms-2 text-sm font-medium text-white bg-blue-700 rounded-lg border border-orange-500  focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                        <button type="submit" class="p-2.5 m-2 text-sm font-medium text-white bg-blue-700 rounded-lg border border-orange-500  focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
                             <svg class="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
                                 <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
                             </svg>
