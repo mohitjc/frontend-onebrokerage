3adccf818d613d12ea8c980a595b48d00719ddd3
Ritika <ritika@jcsoftwaresolution.com>
2024-04-24 12:48:15 +0530

    updated code

--- src/Pages/Events/MemberHistory.js
+++ src/Pages/Events/MemberHistory.js
@@ -8,7 +8,7 @@ const MemberHistory = ({ eventDetail = '' }) => {
       <table className='w-full'>
         <thead className='!border border-[#EAECF0]'>
           <tr className='!border border-[#EAECF0]'>
-            <th className='cursor-pointer text-[#82838B] !border-l-0 font-normal text-sm !border border-[#EAECF0] !px-5 text-left bg-[#F7FAFF] !py-3'>Title</th>
+            <th className='cursor-pointer text-[#82838B] !border-l-0 font-normal text-sm !border border-[#EAECF0] !px-5 text-left bg-[#F7FAFF] !py-3'>Event</th>
             <th className='cursor-pointer text-[#82838B] !border-l-0 font-normal text-sm !border border-[#EAECF0] !px-5 text-left bg-[#F7FAFF] !py-3'>Description</th>
             <th className='cursor-pointer text-[#82838B] !border-l-0 font-normal text-sm !border border-[#EAECF0] !px-5 text-left bg-[#F7FAFF] !py-3'>Type</th>
             <th className='cursor-pointer text-[#82838B] !border-l-0 font-normal text-sm !border border-[#EAECF0] !px-5 text-left bg-[#F7FAFF] !py-3'>Added By</th>
@@ -20,7 +20,7 @@ const MemberHistory = ({ eventDetail = '' }) => {
             return (
               <tr>
                 <td className=' text-typo !px-2 text-sm font-normal !py-3 !border text-center border-[#EAECF0]'>{ele?.eventsDetails?.title}</td>
-                <td className=' text-typo !px-2 text-sm font-normal !py-3 !border text-center border-[#EAECF0]'>{ele?.eventsDetails?.description}</td>
+                <td className=' text-typo !px-2 text-sm font-normal !py-3 !border text-center border-[#EAECF0]'> <div dangerouslySetInnerHTML={{ __html: ele?.eventsDetails?.description }} /></td>
                 <td className=' text-typo !px-2 text-sm font-normal !py-3 !border text-center border-[#EAECF0]'>{ele?.eventsDetails?.type}</td>
                 <td className=' text-typo !px-2 text-sm font-normal !py-3 !border text-center border-[#EAECF0]'>{ele?.emailFrom}</td>
                 <td className=' text-typo !px-2 text-sm font-normal !py-3 !border text-center border-[#EAECF0]'>{ele?.eventsDetails?.address}</td>
