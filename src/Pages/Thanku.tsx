dc10dcb4ac7090dac12fbf1311a074531bf72c37
JC Software Solution <engjcsoftwaresolution@gmail.com>
2024-04-25 15:07:34 +0530

    build deplot

--- src/Pages/Thanku.tsx
+++ src/Pages/Thanku.tsx
@@ -1,6 +1,45 @@
+import { useState } from "react";
 import PageLayout from "../components/global/PageLayout";
+import { BlockBlobClient, AnonymousCredential } from '@azure/storage-blob';
 
 const Thanku = () => {
+  const sasKey:any='sp=racwdli&st=2024-04-25T09:28:38Z&se=2024-04-30T17:28:38Z&sv=2022-11-02&sr=c&sig=qfRZ%2B%2Bb7z9aBHmlcE2XM0XQDIKa2WLcieACpb%2B4S9PU%3D'
+  const sasurl:any='https://skillprobackendstore.blob.core.windows.net';
+  function buildBlobName(file:any) {
+    var filename = file.name.substring(0, file.name.lastIndexOf('.'));
+    var ext = file.name.substring(file.name.lastIndexOf('.'));
+    return filename + '_' + Math.random().toString(16).slice(2) + ext;
+}
+
+const [loading,setLoader]=useState(false)
+
+const blobUpload = function(file:any, url:any=sasurl, container:any='exel', sasKeys=sasKey) {
+    var blobName = buildBlobName(file);
+    var login = `${url}/${container}/${blobName}?${sasKeys}`;
+    var blockBlobClient = new BlockBlobClient(login, new AnonymousCredential());
+    setLoader(true)
+    blockBlobClient.uploadBrowserData(file).then(res=>{
+
+      let url=res._response.request.url
+      console.log("res",res)
+      console.log("url",url)
+      setLoader(false)
+    }).catch(err=>{
+      console.log("err",err)
+      setLoader(false)
+    });
+}
+
+const uploadFile=(e:any)=>{
+  let files=e.target.files;
+  let file=files.item(0)
+  console.log("file",file)
+  if(file){
+    blobUpload(file)
+  }
+}
+
+
   return (
     <>
       <PageLayout>
@@ -14,6 +53,10 @@ const Thanku = () => {
                       </div>
                      </div>
                      
+                     <input type="file" onChange={uploadFile} />
+                     {loading?<>
+                     <div className="text-center">Uploading...</div>
+                     </>:<></>}
                 </div>
           </div>
       </PageLayout>
