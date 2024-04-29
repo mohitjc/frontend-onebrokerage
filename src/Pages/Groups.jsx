a53289fab527b65f12d0a0a21b6c1d5e38868ee4
Ritika <ritika@jcsoftwaresolution.com>
2024-04-29 19:41:15 +0530

    updated data

--- src/Pages/Groups.jsx
+++ src/Pages/Groups.jsx
@@ -18,34 +18,38 @@ import { FiPlus } from 'react-icons/fi';
 import { useLocation } from 'react-router-dom';
 import PageLayout from '../components/global/PageLayout';
 import { toast } from 'react-toastify';
-const Groups = ({ eventDetail = '', dataa = '' }: any) => {
+import datepipeModel from '../models/datepipemodel';
+const Groups = ({ eventDetail = '', dataa = '' }) => {
     console.log(eventDetail, "eventDetaileventDetaileventDetaileventDetaileventDetail")
     const { id } = useParams()
     const location = useLocation();
     const history = useNavigate();
     const receivedStateData = location.state;
     console.log(receivedStateData, "receivedStateDatareceivedStateDatareceivedStateDatareceivedStateDatareceivedStateData")
-    const [groupList, setGroupList]: any = useState()
+    const [groupList, setGroupList] = useState()
     const [attendeesGroup, setattendeesGroup] = useState()
+    const [selectedEmails, setSelectedEmails] = useState([]);
     const [requestGroup, setRequestGroup] = useState(false)
-    const [requestGroupList, setRequestGroupList]: any = useState("")
+    const [requestGroupList, setRequestGroupList] = useState("")
+    const [historyData, setHistoryData] = useState()
     const [data, setData] = useState()
     const [showDiv, setShowDiv] = useState(false)
     const eventIdd = receivedStateData?.eventId?.id
     const groupId = receivedStateData?.dataa?.data?.groupId?._id
+    
     console.log(eventIdd, "eventIddeventIdd")
     console.log(groupList,"groupListgroupListgroupListgroupListgroupList")
 console.log(requestGroupList,"requestGroupListrequestGroupListrequestGroupListrequestGroupListrequestGroupListrequestGroupList")
 const handleCloseModal =()=>{
     setShowDiv(false)
 }
-const importFile = (e : any) => {
+const importFile = (e ) => {
     const file = e.target.files[0];
 
     if (file) {
         const reader = new FileReader();
 
-        reader.onload = (event :any) => {
+        reader.onload = (event ) => {
             const base64String = event.target.result;
 
             let payload = {
@@ -65,7 +69,7 @@ const importFile = (e : any) => {
     }
 };
 
-    const handleSelectValue = (e: any, element: any) => {
+    const handleSelectValue = (e, element) => {
         loader(true)
         let payload = {
             data: [{
@@ -116,7 +120,7 @@ const importFile = (e : any) => {
             var blob = new Blob([res.data], {
                 type: res.headers["content-type"],
             });
-            let downloadAnchor: any = document.createElement("a")
+            let downloadAnchor = document.createElement("a")
             downloadAnchor.href = window.URL.createObjectURL(blob);
             downloadAnchor.download = `GroupsDetail.xlsx`;
             downloadAnchor.click()
@@ -149,7 +153,7 @@ const importFile = (e : any) => {
             var blob = new Blob([res.data], {
                 type: res.headers["content-type"],
             });
-            let downloadAnchor: any = document.createElement("a")
+            let downloadAnchor = document.createElement("a")
             downloadAnchor.href = window.URL.createObjectURL(blob);
             downloadAnchor.download = `SampleFile.xlsx`;
             downloadAnchor.click()
@@ -161,7 +165,30 @@ const importFile = (e : any) => {
 
 
     };
-    console.log(groupList, "groupList")
+    const handleCheckboxClick = (e, email) => {
+        if (e.target.checked) {
+            setSelectedEmails(prevSelectedEmails => [...prevSelectedEmails, email]);
+        } else {
+            setSelectedEmails(prevSelectedEmails => prevSelectedEmails.filter(selectedEmail => selectedEmail !== email));
+        }
+    };
+    const handleSelectAllClick = (e, attendees) => {
+        const emails = attendees.map(attendee => attendee.attendeesDetails.email);
+        if (e.target.checked) {
+            setSelectedEmails(prevSelectedEmails => [...prevSelectedEmails, ...emails]);
+        } else {
+            setSelectedEmails(prevSelectedEmails => prevSelectedEmails.filter(selectedEmail => !emails.includes(selectedEmail)));
+        }
+    };
+    const getselectedEmailsData=()=>{
+        const emails = selectedEmails.join(',');
+        ApiClient.get(`api/import/event-group/list?email=${emails}`).then(res => {
+            if (res.success) {
+                setHistoryData(res?.data)
+            }
+        })
+    }
+    console.log(historyData,"historyData")
     return (
         <>
 
@@ -182,6 +209,7 @@ const importFile = (e : any) => {
             </span>
             Back
           </div>
+          <buuton onClick={getselectedEmailsData}>Search</buuton>
                         <div className=''>
                             <div className='flex items-center gap-4  mb-3'>
 
@@ -229,7 +257,7 @@ const importFile = (e : any) => {
                                 <button className='bg-orange-500 text-sm px-2 py-1 flex items-center gap-1 rounded-md text-white' onClick={handleCloseModal}><MdClear />  Close</button>
                                 </div>
                                 <div className='grid grid-cols-1 lg:grid-cols-2'>
-                                    {requestGroupList?.length ? requestGroupList?.map((ele: any) => {
+                                    {requestGroupList?.length ? requestGroupList?.map((ele) => {
                                         return (<><div className="shwoing-requeted">
                                             <h4 className='mb-4 mt-4 text-xs'>Requested By : <span className='bg-orange-500 px-2 py-1 pb-1 text-sm rounded-md ml-2 text-white'>{ele?.requestedBy}</span></h4>
 
@@ -241,7 +269,7 @@ const importFile = (e : any) => {
                                                     </tr>
                                                 </thead>
                                                 <tbody>
-                                                    {ele?.attendeesDetails?.map((element: any) => {
+                                                    {ele?.attendeesDetails?.map((element) => {
                                                         return (<tr>
                                                             <td className='border border-gray-200 p-2'>{element?.fullName}</td>
                                                        <td className='border border-gray-200 p-2'>{element?.email}</td>
@@ -263,24 +291,25 @@ const importFile = (e : any) => {
                             </div> : ""}
                             
                             <div className='grid grid-cols-12 gap-x-4'>
-                                    <div className='col-span-12 md:col-span-8'>
+                                    <div className='col-span-12 md:col-span-12'>
 
-                                        <div className='grid grid-cols-1 lg:grid-cols-2 gap-2'>
-                                            {groupList?.sort((a: any, b: any) => a.groupNo - b.groupNo)?.map((ele: any) => {
+                                        <div className='grid grid-cols-1 lg:grid-cols-2  gap-2'>
+                                           
+                                            {groupList?.sort((a, b) => a.groupNo - b.groupNo)?.map((ele) => {
                                                 return (
                                                     <>
-
+ 
                                                         <div className='brouplists border border-1 rounded-lg shadow-lg p-3 mb-3'>
 
                                                             <div className='flex items-center justify-between border-b border-1 pb-2 mb-2'>
-                                                                <h2 className='font-bold text-[#2b2b2b] text-lg'> Group{ele?.groupNo}</h2>
+                                                                <h2 className='font-bold text-[#2b2b2b] text-lg'> Group{ele?.groupNo}</h2>  <input type="checkbox" onChange={(e) => handleSelectAllClick(e, ele.attendees)} />
                                                                 {ele?.isUnique === true ? <span className='bg-orange-500 p-2 text-xs  mb-0 text-white rounded-md inline-flex'>New Pair</span> : ""}
                                                             </div>
-                                                            <p className=''>{ele?.attendees?.map((element: any) => {
+                                                            <p className=''>{ele?.attendees?.map((element) => {
                                                                 return (
                                                                     <div className='card_inners border-b  last:border-0  items-center flex  flex-wrap gap-y-2 items-center justify-between  p-2'>
                                                                         <div className='flex items-start gap-1'>
-                                                                            <input type="checkbox" className='mt-1' />     
+                                                                            {/* <input type="checkbox" className='mt-1'onChange={(e) => handleCheckboxClick(e, element?.attendeesDetails?.email)} />      */}
                                                                         <div>
                                                                             {element?.attendeesDetails?.fullName ? <p className='flex items-center mb-2 gap-1 text-[#75757A] capitalize text-[14px] '><LuUser2 className='text-black !text-[16px]' />{element?.attendeesDetails?.fullName}</p> : ""}
                                                                             {element?.attendeesDetails?.email ? <p className='flex items-center gap-1 text-[#75757A]  text-[14px]'><MdOutlineEmail className='text-black !text-[16px]' />{element?.attendeesDetails?.email}</p> : ""}
@@ -288,9 +317,9 @@ const importFile = (e : any) => {
                                                                         </div>
                                                                         </div>
                                                                         {element?.attendeesDetails?.fullName || element?.attendeesDetails?.email ? <div className='rounded-full shdaow-md bg-orange-400 focus:outline-none cursor-pointer  px-2 pb-1 '>
-                                                                            <select className=" bg-transparent focus:outline-none cursor-pointer text-[14px]  text-white" onChange={(e: any) => handleSelectValue(e, element)}>
+                                                                            <select className=" bg-transparent focus:outline-none cursor-pointer text-[14px]  text-white" onChange={(e) => handleSelectValue(e, element)}>
                                                                                 <option value="" disabled selected>Move to</option>
-                                                                                {groupList?.filter((data: any) => data.groupNo !== ele.groupNo)?.map((data: any) => (
+                                                                                {groupList?.filter((data) => data.groupNo !== ele.groupNo)?.map((data) => (
                                                                                     <option className='text-md text-black' key={data?.groupNo} value={data?._id?.ConnectMeetId}>Group {data?.groupNo}</option>
                                                                                 ))}
                                                                             </select>
@@ -309,7 +338,7 @@ const importFile = (e : any) => {
                                         </div>
 
                                     </div>
-                                    <div className='col-span-12 md:col-span-4'>
+                                    {/* <div className='col-span-12 md:col-span-4'>
                                         <div className='histroy_showinf p-4 rounded-md border border-gray-300'>
                                             <h6 className='text-black text-xl flex items-center gap-2  mb-4'><GoHistory /> History</h6>
                                             
@@ -328,7 +357,7 @@ const importFile = (e : any) => {
                                                                 <Disclosure.Button className="flex w-full items-center cursor-pointer justify-between rounded-lg bg-orange-100 px-4 py-2 text-left text-sm font-medium text-purple-900 hover:bg-orange-200 focus:outline-none focus-visible:ring focus-visible:ring-purple-500/75">
                                                                     <div className=' '>
                                                                         <div className='flex flex-col gap-y-1'>
-                                                                            <h4 className='text-lg font-bold text-[#2b2b2b]'>Event Name</h4>
+                                                                            <h4 className='text-lg font-bold text-[#2b2b2b]'> Name </h4>
                                                                             <p className='text-sm text-gray-500'>11:30pm - <span>1/02/2022</span></p>
                                                                         </div>
                                                                     </div>
@@ -347,7 +376,27 @@ const importFile = (e : any) => {
                                                                 </>
                                                             )}
                                                             </Disclosure>
+                                                            {historyData?.map((ele)=>{
+return(<div><h4>Name{ele?.fullName}</h4>
+<h4>E-mail{ele?.email}</h4>
+<div>
+    <h5>{historyData?.eventDetail?.map((element)=>{
+        return(
+            <>
+               <p> EventName{element?.
+                eventName
+                }</p>
+                <p>EventDate{datepipeModel.datetime(element?.eventDate)} </p>
+                <p>connectMeetDate {datepipeModel.datetime(element?.connectMeetDate)} </p>
+            </>
+         
+        )
+    })}</h5>
+    </div>
+</div>)
+                                                            })}
                                                             
+                                                            <div></div>
                                                         </div>
                                                 </div>
 
@@ -358,7 +407,7 @@ const importFile = (e : any) => {
 
                                         </div>
 
-                                    </div>
+                                    </div> */}
                             </div>
 
 
@@ -366,7 +415,7 @@ const importFile = (e : any) => {
                         {/* {Object.entries(eventDetail).map(([group, items] :any) => (
                 <div key={group}>
                     <p>{group}</p>
-                    {items?.map((item: any) => (
+                    {items?.map((item) => (
                         <div key={item.id} style={{ display: "flex", justifyContent: "space-evenly" }}>
                             <p>{item.fullName}</p>
                             <p>{item.email}</p>
