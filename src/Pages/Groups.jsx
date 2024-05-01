9615d1874316d63c92bb24b58d184f5ddf8fa4cf
Frontend RVRenterCenter <frontend@rvrentercenter.com>
2024-05-01 16:50:53 +0500

    chnages for the skills

--- src/Pages/Groups.jsx
+++ src/Pages/Groups.jsx
@@ -393,7 +393,7 @@ const Groups = ({ eventDetail = '', dataa = '' }) => {
                                                                         <div className=' '>
 
                                                                             <div className='flex flex-col gap-y-1'>
-                                                                                <h4 className='text-md font-bold text-[#2b2b2b]'>{ele?.eventsDetails?.[0]?.title}</h4>
+                                                                                <h4 className='text-md font-bold text-[#2b2b2b] break-all'>{ele?.eventsDetails?.[0]?.title}</h4>
                                                                                 <p className='text-xs text-gray-500'>{ele?.ConnectMeetId}</p>
                                                                             </div>
                                                                         </div>
