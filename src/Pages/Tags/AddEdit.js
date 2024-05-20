6991ed66148db6ef813a493fb662ec096ab427c5
Vishal Tyagi <vishaltyagi@jcsoftwaresolution.com>
2024-05-20 19:29:06 +0530

    UI changes

--- src/Pages/Tags/AddEdit.js
+++ src/Pages/Tags/AddEdit.js
@@ -27,9 +27,9 @@ const AddEdit = () => {
   const [submitted, setSubmitted] = useState(false);
   const user = useSelector((state) => state.user);
   const formValidation = [
-    /*  { key: "status", required: true },
+    /*     { key: "status", required: true }, */
     { key: "type", required: true, message: "Type is required" },
-    { key: "timezone", required: true },
+    /* { key: "timezone", required: true },
     { key: "description", required: true, message: "Description is required" }, */
     // { key:'groupMemberLimit' , required:true ,message:'Group Member Limit is required'}
   ];
@@ -157,6 +157,9 @@ const AddEdit = () => {
                   theme="search"
                   required
                 />
+                {submitted && !form.type && (
+                  <span className="text-danger">type is required.</span>
+                )}
               </div>
 
               <div className="mb-3">
