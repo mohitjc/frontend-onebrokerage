d1f076f8d96f79876d3b5d2dc7cdbb3d1e22b682
Vishal Tyagi <vishaltyagi@jcsoftwaresolution.com>
2024-05-27 16:49:56 +0530

    UI Changes

--- src/Pages/Signup/index.tsx
+++ src/Pages/Signup/index.tsx
@@ -1,126 +1,133 @@
-import React, { useEffect, useState } from 'react';
-import {  useNavigate } from 'react-router-dom';
-import ApiClient from '../../methods/api/apiClient';
-import loader from '../../methods/loader';
-import { Link } from 'react-router-dom';
-import './style.scss';
-import AuthLayout from '../../components/AuthLayout';
-import environment from '../../environment';
-import { toast } from 'react-toastify';
-import methodModel from '../../methods/methods';
-import FormControl from '../../components/common/FormControl';
-import { useDispatch, useSelector } from 'react-redux';
-import { login_success } from '../actions/user';
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import ApiClient from "../../methods/api/apiClient";
+import loader from "../../methods/loader";
+import { Link } from "react-router-dom";
+import "./style.scss";
+import AuthLayout from "../../components/AuthLayout";
+import environment from "../../environment";
+import { toast } from "react-toastify";
+import methodModel from "../../methods/methods";
+import FormControl from "../../components/common/FormControl";
+import { useDispatch, useSelector } from "react-redux";
+import { login_success } from "../actions/user";
 
 const Signup = () => {
   const history = useNavigate();
-  const user = useSelector((state:any) => state.user);
- const dispatch=useDispatch()
+  const user = useSelector((state: any) => state.user);
+  const dispatch = useDispatch();
 
-  const [form, setForm]:any = useState({ email: '', password: '', fullName: '',customerRole:environment.customerRoleId ,loginId:""});
+  const [form, setForm]: any = useState({
+    email: "",
+    password: "",
+    fullName: "",
+    customerRole: environment.customerRoleId,
+    loginId: "",
+  });
   const [remember, setRemember] = useState(false);
   const [submitted, setSubmitted] = useState(false);
   const [groups, setGroups] = useState([]);
-  const [eyes, setEyes] = useState({ password: false, confirmPassword: false, currentPassword: false });
+  const [eyes, setEyes] = useState({
+    password: false,
+    confirmPassword: false,
+    currentPassword: false,
+  });
 
-
-  const setLogin=async(data:any)=>{
-  
-    let url = '/profile'
-    let eventId=methodModel.getPrams('eventId')
+  const setLogin = async (data: any) => {
+    let url = "/profile";
+    let eventId = methodModel.getPrams("eventId");
     if (eventId) {
-      if(methodModel.getPrams('attended')){
+      if (methodModel.getPrams("attended")) {
         try {
-          const res = await ApiClient.get(`api/attandance?email=${data?.email}&eventId=${eventId}`);
+          const res = await ApiClient.get(
+            `api/attandance?email=${data?.email}&eventId=${eventId}`
+          );
           console.log(res.success, "res.success");
           if (res.success === true) {
-              url = `/thanku`
+            url = `/thanku`;
           }
-      } catch (error) {
+        } catch (error) {
           console.error("Error fetching attendance:", error);
-      }
-      }else{
-        url = `/event/detail/${eventId}`
+        }
+      } else {
+        url = `/event/detail/${eventId}`;
       }
     }
-    localStorage.setItem('token', data.access_token)
+    localStorage.setItem("token", data.access_token);
     dispatch(login_success(data));
     history(url);
-  }
+  };
 
-  const getGroups=()=>{
-    ApiClient.get('api/group/list',{status:'active'}).then((res:any)=>{
-      if(res.success){
-        setGroups(res.data)
+  const getGroups = () => {
+    ApiClient.get("api/group/list", { status: "active" }).then((res: any) => {
+      if (res.success) {
+        setGroups(res.data);
       }
-    })
-  }
- 
+    });
+  };
+
   const hendleSubmit = (e: any) => {
-    e.preventDefault()
-    setSubmitted(true)
+    e.preventDefault();
+    setSubmitted(true);
 
-    let url='api/user/register'
-    let eventId=methodModel.getPrams('eventId')
-    let groupId=methodModel.getPrams('groupId')
+    let url = "api/user/register";
+    let eventId = methodModel.getPrams("eventId");
+    let groupId = methodModel.getPrams("groupId");
 
-    if(!remember) return
+    if (!remember) return;
 
-    let data:any = {
-      role:environment.userRoleId,
-      ...form
+    let data: any = {
+      role: environment.userRoleId,
+      ...form,
     };
 
-    if(groupId) data.groupId=groupId
-    if(data.customerRole==environment.glRoleId){
-      if(!data.groupId){
-        return
+    if (groupId) data.groupId = groupId;
+    if (data.customerRole == environment.glRoleId) {
+      if (!data.groupId) {
+        return;
       }
     }
 
-    loader(true)
-  
-    
-    ApiClient.post(url, data).then(async res => {
-   
+    loader(true);
+
+    ApiClient.post(url, data).then(async (res) => {
       if (res.success) {
-        if(eventId||groupId){
-          await ApiClient.post('api/auto/login',{id:res.data._id}).then(async res=>{
-            setLogin(res.data)
-          })
-          
-        }else{
-          let url = '/login'
-          setTimeout(()=>{
-            toast.success("Please verify your email")
-          },400)
+        if (eventId || groupId) {
+          await ApiClient.post("api/auto/login", { id: res.data._id }).then(
+            async (res) => {
+              setLogin(res.data);
+            }
+          );
+        } else {
+          let url = "/login";
+          setTimeout(() => {
+            toast.success("Please verify your email");
+          }, 400);
           history(url);
         }
-       
       }
-      loader(false)
-    })
+      loader(false);
+    });
   };
 
   useEffect(() => {
-    let email=methodModel.getPrams('email')
+    let email = methodModel.getPrams("email");
     if (user && user?.loggedIn) {
-      history('/dashboard')
+      history("/dashboard");
     }
 
-    
-    if(email){
+    if (email) {
       setForm({
         ...form,
-        email:email,
-        fullName:methodModel.getPrams('name'),
-      })
+        email: email,
+        fullName: methodModel.getPrams("name"),
+      });
     }
 
-    getGroups()
-  }, [])
-  const [loginIdExists, setLoginIdExists] = useState(true); 
-  const getLoginId = (loginId :any) => {
+    getGroups();
+  }, []);
+  const [loginIdExists, setLoginIdExists] = useState(true);
+  const getLoginId = (loginId: any) => {
     ApiClient.get(`api/check/login-id?loginId=${loginId ? loginId : null}`)
       .then((res) => {
         if (res.success) {
@@ -130,12 +137,12 @@ const Signup = () => {
         }
       })
       .catch((error) => {
-        console.error('Error checking login ID:', error);
-        setLoginIdExists(true); 
+        console.error("Error checking login ID:", error);
+        setLoginIdExists(true);
       });
   };
-  
-  const handleLoginIdChange = (e :any) => {
+
+  const handleLoginIdChange = (e: any) => {
     const { value } = e.target;
     setForm({ ...form, loginId: value });
     getLoginId(value);
@@ -143,7 +150,7 @@ const Signup = () => {
   return (
     <>
       <AuthLayout>
-        <form className="" onSubmit={hendleSubmit} autoComplete='off'>
+        <form className="" onSubmit={hendleSubmit} autoComplete="off">
           <h4 className="text-typo mb-6 text-2xl font-bold">Sign Up</h4>
           {/* <input type="text"
             onChange={e => setForm({ ...form, loginId: e.target.value })}
@@ -153,107 +160,154 @@ const Signup = () => {
             autoComplete='off'
             disabled={methodModel.getPrams('attended')?true:false}
             required /> */}
-              <input
-      type="text"
-      onChange={handleLoginIdChange}
-      value={form.loginId}
-      className={`shadow-box border-1 border-gray-300 relative bg-gray-100 mb-3 w-full text-sm placeholder:text-gray-500 rounded-lg h-12 flex items-center gap-2 overflow-hidden px-2 hover:ring-orange-500 focus:border-[#EB6A59]${loginIdExists ? '' : 'border-red-500'}`}
-      placeholder="Login Id"
-      autoComplete="off"
-      maxLength={8}
-      disabled={methodModel.getPrams('attended') ? true : false}
-      required
-    />
-      {loginIdExists ? null : <p className="text-red-500">Login ID already exists.</p>}
+          <input
+            type="text"
+            onChange={handleLoginIdChange}
+            value={form.loginId}
+            className={`shadow-box border-1 border-gray-300 relative bg-gray-100 mb-3 w-full text-sm placeholder:text-gray-500 rounded-lg h-12 flex items-center gap-2 overflow-hidden px-2 hover:ring-orange-500 focus:border-[#EB6A59]${
+              loginIdExists ? "" : "border-red-500"
+            }`}
+            placeholder="Login Id"
+            autoComplete="off"
+            maxLength={8}
+            disabled={methodModel.getPrams("attended") ? true : false}
+            required
+          />
+          {loginIdExists ? null : (
+            <p className="text-red-500">Login ID already exists.</p>
+          )}
 
-          <input type="email"
-            onChange={e => setForm({ ...form, email: e.target.value })}
+          <input
+            type="email"
+            onChange={(e) => setForm({ ...form, email: e.target.value })}
             value={form.email}
             className="shadow-box border-1 border-gray-300 relative bg-gray-100 mb-3 w-full text-sm placeholder:text-gray-500 rounded-lg h-12 flex items-center gap-2 overflow-hidden px-2 hover:ring-orange-500 focus:border-orange-500"
             placeholder="Email address"
-            autoComplete='off'
-            disabled={methodModel.getPrams('attended')?true:false}
-            required />
-             <input type="text"
-            onChange={e => setForm({ ...form, fullName: e.target.value })}
+            autoComplete="off"
+            disabled={methodModel.getPrams("attended") ? true : false}
+            required
+          />
+          <input
+            type="text"
+            onChange={(e) => setForm({ ...form, fullName: e.target.value })}
             value={form.fullName}
             className="shadow-box border-1 border-gray-300 relative bg-gray-100 mb-3 w-full text-sm placeholder:text-gray-500 rounded-lg h-12 flex items-center gap-2 overflow-hidden px-2 hover:ring-orange-500 focus:border-orange-500"
             placeholder="Enter Name"
-            autoComplete='off'
-            required />
+            autoComplete="off"
+            required
+          />
           <div className="relative mb-6">
-            <input 
-            type={eyes.password ? 'text' : 'password'} 
-            className="shadow-box border-1 border-gray-300 relative bg-gray-100 w-full text-sm placeholder:text-gray-500 rounded-lg h-12 flex items-center gap-2 overflow-hidden px-2 hover:ring-orange-500 focus:border-orange-500" placeholder="Password"
-             onChange={e => setForm({ ...form, password: e.target.value })}
-             value={form.password}
-             minLength={8}
-             autoComplete='off'
-            required />
-
-            <div className='absolute right-2 inset-y-0 flex items-center text-gray-500 text-sm'>
-              <i className={eyes.password ? 'fa fa-eye' : 'fa fa-eye-slash'} onClick={() => setEyes({ ...eyes, password: !eyes.password })}></i>
-
-            </div>
-
-          </div>
-          
-{methodModel.getPrams('eventId')||methodModel.getPrams('groupId')?<>
-
-</>:<>
-<div className='mb-3'>
-            <FormControl
-            type='radio'
-            options={[
-              {id:environment.customerRoleId,name:'No'},
-              {id:environment.glRoleId,name:'Yes'},
-            ]}
-            label="Do You Want To Be A Group Leader"
-            value={form.customerRole}
-            onChange={(e:any)=>{
-              setForm({...form,customerRole:e,groupId:null})
-            }}
+            <input
+              type={eyes.password ? "text" : "password"}
+              className="shadow-box border-1 border-gray-300 relative bg-gray-100 w-full text-sm placeholder:text-gray-500 rounded-lg h-12 flex items-center gap-2 overflow-hidden px-2 hover:ring-orange-500 focus:border-orange-500"
+              placeholder="Password"
+              onChange={(e) => setForm({ ...form, password: e.target.value })}
+              value={form.password}
+              minLength={8}
+              autoComplete="off"
+              required
             />
-          </div>
-</>}
-          
 
-{form.customerRole==environment.glRoleId?<>
-  <div className='mb-3 relative'>
-           
-           <FormControl
-           type='select'
-           options={groups}
-           displayValue="name"
-           placeholder="Select Group"
-           label="Group"
-           value={form.groupId}
-           onChange={(e:any)=>{
-             setForm({...form,groupId:e})
-           }}
-           />
-           {submitted&&!form.groupId?<>
-           <div className='text-[#EB6A59] text-sm'>Group is required</div>
-           </>:<></>}
-         </div>
-</>:<></>}
-          
-          <div className='flex'>
-            <label className='flex items-center pointer'><input type="checkbox" checked={remember} onChange={(e) => setRemember(e.target.checked)} className="mr-2 h-4 w-4" /> <span className='text-xs text-gray-600'>By clicking Create account, I agree that I have read and accepted the Terms of Use and Privacy Policy.</span></label>
+            <div className="absolute right-2 inset-y-0 flex items-center text-gray-500 text-sm">
+              <i
+                className={eyes.password ? "fa fa-eye" : "fa fa-eye-slash"}
+                onClick={() => setEyes({ ...eyes, password: !eyes.password })}
+              ></i>
+            </div>
+          </div>
+
+          {methodModel.getPrams("eventId") ||
+          methodModel.getPrams("groupId") ? (
+            <></>
+          ) : (
+            <>
+              <div className="mb-3">
+                <FormControl
+                  type="radio"
+                  options={[
+                    { id: environment.customerRoleId, name: "No" },
+                    { id: environment.glRoleId, name: "Yes" },
+                  ]}
+                  label="Do You Want To Be A Group Leader"
+                  value={form.customerRole}
+                  onChange={(e: any) => {
+                    setForm({ ...form, customerRole: e, groupId: null });
+                  }}
+                />
+              </div>
+            </>
+          )}
+
+          {form.customerRole == environment.glRoleId ? (
+            <>
+              <div className="mb-3 relative">
+                <FormControl
+                  type="select"
+                  options={groups}
+                  displayValue="name"
+                  placeholder="Select Group"
+                  label="Group"
+                  value={form.groupId}
+                  onChange={(e: any) => {
+                    setForm({ ...form, groupId: e });
+                  }}
+                />
+                {submitted && !form.groupId ? (
+                  <>
+                    <div className="text-[#EB6A59] text-sm">
+                      Group is required
+                    </div>
+                  </>
+                ) : (
+                  <></>
+                )}
+              </div>
+            </>
+          ) : (
+            <></>
+          )}
+
+          <div className="flex">
+            <label className="flex items-center pointer">
+              <input
+                type="checkbox"
+                checked={remember}
+                onChange={(e) => setRemember(e.target.checked)}
+                className="mr-2 h-4 w-4"
+              />{" "}
+              <span className="text-xs text-gray-600">
+                By clicking Create account, I agree that I have read and
+                accepted the Terms of Use and Privacy Policy.
+              </span>
+            </label>
             {/* <Link className="sign_up ml-auto text-primary" to="/forgotpassword"> Forgot Password</Link> */}
           </div>
 
-          {submitted&&!remember?<>
-           <div className='text-[#EB6A59] text-sm capitalize mt-3'>Please agree our Terms Of Use And Privacy Policy</div>
-           </>:<></>}
-
+          {submitted && !remember ? (
+            <>
+              <div className="text-[#EB6A59] text-sm capitalize mt-3">
+                Please agree our Terms Of Use And Privacy Policy
+              </div>
+            </>
+          ) : (
+            <></>
+          )}
 
           <div className="mt-8">
-            <button type="submit" className="px-4 w-full text-sm font-normal text-white h-12 flex items-center justify-center gap-2 !bg-[#EB6A59] rounded-lg shadow-btn hover:opacity-80 transition-all focus:ring-2 ring-[#EDEBFC] disabled:bg-[#D0CAF6] disabled:cursor-not-allowed">Sign Up</button>
+            <button
+              type="submit"
+              className="px-4 w-full text-sm font-normal text-white h-12 flex items-center justify-center gap-2 !bg-[#EB6A59] rounded-lg shadow-btn hover:opacity-80 transition-all focus:ring-2 ring-[#EDEBFC] disabled:bg-[#D0CAF6] disabled:cursor-not-allowed"
+            >
+              Sign Up
+            </button>
           </div>
 
-          <p className='text-sm mt-3 text-center'>Already have an account? <Link to="/login" className='text-[#EB6A59] text-sm'>Sign In</Link></p>
-
+          <p className="text-sm mt-3 text-center">
+            Already have an account?{" "}
+            <Link to="/login" className="text-[#EB6A59] text-sm">
+              Sign In
+            </Link>
+          </p>
         </form>
       </AuthLayout>
     </>
