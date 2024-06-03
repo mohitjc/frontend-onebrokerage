12dda5ba98374c70debf76ac95adbca742df03cf
Raj Saini <rajrani@jcsoftwaresolution.com>
2024-06-03 19:36:53 +0530

    chnages for the designs

--- src/components/AuthLayout/index.tsx
+++ src/components/AuthLayout/index.tsx
@@ -4,7 +4,7 @@ const AuthLayout=({children}:any)=>{
     return <>
       <div className='grid items-center grid-cols-12 bg_images'>
        
-       <div className="col-span-12 md:col-span-5 lg:col-span-5">
+       <div className="col-span-12 md:col-span-5 lg:col-span-5 hidden md:block">
          <div className="relative flex flex-col items-center justify-center h-screen py-8 overflow-auto">
          <div className=" w-full max-w-md px-8 py-6 mx-auto overflow-y-auto rounded-lg">
            <Link to="/"><img src="/assets/img/logo.png" className="w-[35 0px] mb-6 mx-auto" alt="logo" /></Link>
@@ -14,8 +14,8 @@ const AuthLayout=({children}:any)=>{
 
          </div>
        </div>
-       <div className="col-span-12 md:col-span-7 lg:col-span-7 hidden md:block">
-         <div className="relative w-full h-screen">
+       <div className="col-span-12 md:col-span-7 lg:col-span-7 ">
+         <div className="relative w-full h-screen overflow-auto-auto">
            <img src="/assets/img/login_Img.png" alt="bg-logon" width="auto" height="auto" className=" object-center object-cover absolute inset-0 w-full h-full z-10" />
            <div className="flex flex-col justify-between h-full   py-6 px-3 xs:px-4 md:px-4  xl:px-12 xl:py-16 relative z-20">
            <div className="flex items-center justify-center h-full  w-full  lg:pl-14 relative z-20">
