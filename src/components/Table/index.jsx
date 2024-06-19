fe6c1d91379d03a1d8b3572fec20b4aadc06cdd9
deepak <deepak@yopmail.com>
2024-06-19 12:30:22 +0530

    banner page

--- src/components/Table/index.jsx
+++ src/components/Table/index.jsx
@@ -87,14 +87,17 @@ const Table = ({
                           onClick={() => headclick(itm)}
                           key={itm.key}
                         >
-                          {itm.name}{" "}
+                        <span className="inline-flex items-center gap-1">
+
+                        <span>  {itm.name}{" "}</span>
                           {itm.sort ? (
                             <>
-                              <span className="ml-1">
+                              <span className="">
                                 <HiOutlineArrowDown className=" inline text-sm" />
                               </span>
                             </>
                           ) : null}
+                        </span>
                         </th>
                       ))}
                     </tr>
