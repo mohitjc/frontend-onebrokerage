27ffbb4cd4a9b0853dd2aa00b765417071ea832f
amandeep <amandeep@jcsoftwaresolution.com>
2024-06-21 17:22:56 +0530

    UI CHANGES

--- src/Pages/Emails/shared.js
+++ src/Pages/Emails/shared.js
@@ -2,13 +2,14 @@ const shared = {
   check: "Emails",
   title: "Subscribers",
   addTitle: "Subscribes",
-  url: "subscribe",
+  url: "emails",
   addApi: "subscribe/add",
   editApi: "subscribe",
   detailApi: "subscribe/detail",
   listApi: "subscribe/listing",
   statusApi: "subscribe/status/change",
   deleteApi: "subscribe/delete",
+  sendNewsletter: "newsletter/send",
 };
 
 export default shared;
