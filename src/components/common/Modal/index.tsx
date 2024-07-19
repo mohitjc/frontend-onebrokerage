757e3cafd7d9914a61481ffbf2a25806c83c8653
deepak <deepak@yopmail.com>
2024-07-19 17:48:25 +0530

    changes for the videos

--- src/components/common/Modal/index.tsx
+++ src/components/common/Modal/index.tsx
@@ -5,7 +5,7 @@ export default function Modal({result=(e:any)=>{},body=<>test</>,title='Modal ti
     }
 
     return <>
-    <div className="modal overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
+    <div className="modal overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-[999] justify-center items-center w-full md:inset-0  max-h-full">
     <div className={`relative p-4 w-full ${className}  max-h-full mx-auto`}>
 
         <div className="relative bg-white rounded-lg shadow dark:bg-gray-700">
