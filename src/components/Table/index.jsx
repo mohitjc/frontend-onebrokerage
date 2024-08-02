d35b45e8a07bd62a472fed5d79a4762b5764b19b
Frontend RVRenterCenter <frontend@rvrentercenter.com>
2024-08-02 14:34:59 +0530

    add onclick on arrow

--- src/components/Table/index.jsx
+++ src/components/Table/index.jsx
@@ -84,14 +84,13 @@ const Table = ({
                           className={`px-6 py-3   ${
                             itm.sort ? "cursor-pointer" : ""
                           }`}
-                          onClick={() => headclick(itm)}
                           key={itm.key}
                         >
                           <span className="inline-flex items-center gap-1">
                             <span> {itm.name} </span>
                             {itm.sort ? (
                               <>
-                                <span className="">
+                                <span className="" onClick={() => headclick(itm)}>
                                   <HiOutlineArrowDown className=" inline text-sm" />
                                 </span>
                               </>
