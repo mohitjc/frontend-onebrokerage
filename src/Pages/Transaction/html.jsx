be0315bca5e4735ff110c4522ee96443926407fc
Pooja Sharma <poojas@jcsoftwaresolution.com>
2024-09-02 15:26:52 +0530

    add search functionality in trasaction

--- src/Pages/Transaction/html.jsx
+++ src/Pages/Transaction/html.jsx
@@ -13,11 +13,14 @@ const Html = ({
   isAllow,
   reset,
   view,
+  clear,
   ChangeStatus,
   statusChange,
   handleFilter,
   pageChange,
   filters,
+  setFilter,
+  filter,
   handleFilters,
   loaging,
   data,
@@ -109,6 +112,63 @@ const Html = ({
       <div className="shadow-box w-full bg-white rounded-lg mt-6">
 
       <div className="flex gap-2 ml-auto">
+      <form
+            class="flex items-center max-w-sm"
+            onSubmit={(e) => {
+              e.preventDefault();
+              filter();
+            }}
+          >
+            <label for="simple-search" class="sr-only">
+              Search
+            </label>
+            <div class="relative w-full">
+              {/* <div class="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+                                <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 20">
+                                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 5v10M3 5a2 2 0 1 0 0-4 2 2 0 0 0 0 4Zm0 10a2 2 0 1 0 0 4 2 2 0 0 0 0-4Zm12 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4Zm0 0V6a3 3 0 0 0-3-3H9m1.5-2-2 2 2 2"/>
+                                </svg>
+                            </div> */}
+              <input
+                type="text"
+                id="simple-search"
+                value={filters.search}
+                onChange={(e) => {
+                  setFilter({ ...filters, search: e.target.value });
+                }}
+                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-orange-500 focus:border-[#494f9f]block w-full ps-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-orange-500 dark:focus:border-orange-500 pr-10"
+                placeholder="Search"
+                // required
+              />
+              {filters?.search && (
+                <i
+                  className="fa fa-times absolute right-4 top-1/2 -translate-y-1/2 text-gray-500 text-sm"
+                  aria-hidden="true"
+                  onClick={(e) => clear()}
+                ></i>
+              )}
+            </div>
+            <button
+              type="submit"
+              class="p-2.5 m-2 text-sm font-medium text-white bg-blue-700 rounded-lg border border-[#494f9f] focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+            >
+              <svg
+                class="w-4 h-4"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 20 20"
+              >
+                <path
+                  stroke="currentColor"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
+                />
+              </svg>
+              <span class="sr-only">Search</span>
+            </button>
+          </form>
       <Select className="rounded-lg" options={statusOptions} value={filters?.transaction_status} onChange={e => handleFilters (e, 'transaction_status')} isClearable placeholder='Select Status' />
             {filters.transaction_status ? (
               <>
