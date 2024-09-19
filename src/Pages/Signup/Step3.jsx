158a0823b10e4a8dde2a16c058e2e64216de638a
upakshi <upakshi@jcsoftwaresolution.com>
2024-09-19 19:07:55 +0530

    add modal

--- src/Pages/Signup/Step3.jsx
+++ src/Pages/Signup/Step3.jsx
@@ -10,6 +10,10 @@ import "react-step-progress-bar/styles.css";
 import { ProgressBar, Step } from "react-step-progress-bar";
 import axios, { Axios } from "axios";
 import AuthLayout from "../../components/AuthLayout";
+import { Dialog, Transition } from '@headlessui/react'
+import { Fragment } from 'react'
+
+
 // import { requestForToken } from "../../firebase/configuration";
 export default function Step3() {
   const [percent, setPercent] = useState(100);
@@ -137,6 +141,7 @@ useEffect(() => {
       })(window.lintrk);
     `;
     document.head.appendChild(script);
+    
 
     // Add the noscript image
     const noscriptImg = document.createElement('img');
@@ -153,6 +158,20 @@ useEffect(() => {
       document.body.removeChild(noscriptImg);
     };
   }, []);
+
+
+  let [isOpen, setIsOpen] = useState(false)
+
+  function closeModal() {
+    setIsOpen(false)
+  }
+
+  function openModal() {
+    setIsOpen(true)
+  }
+
+
+
   return (
     <AuthLayout>
     <div className="">
@@ -536,6 +555,88 @@ useEffect(() => {
                   {/* end tab */}
                 </form>
            
+
+                <div className="fixed inset-0 hidden flex items-center justify-center">
+        <button
+          type="button"
+          id="OpenBidModel"
+          onClick={openModal}
+          className="rounded-md bg-black/20 px-4 py-2 text-sm font-medium text-white hover:bg-black/30 focus:outline-none focus-visible:ring-2 focus-visible:ring-white/75"
+        >
+          Open dialog
+        </button>
+      </div>
+
+
+
+                <Transition appear show={isOpen} as={Fragment}>
+        <Dialog as="div" className="relative z-10" onClose={closeModal}>
+          <Transition.Child
+            as={Fragment}
+            enter="ease-out duration-300"
+            enterFrom="opacity-0"
+            enterTo="opacity-100"
+            leave="ease-in duration-200"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+          >
+            <div className="fixed inset-0 bg-black/25" />
+          </Transition.Child>
+
+          <div className="fixed inset-0 overflow-y-auto">
+            <div className="flex min-h-full items-center justify-center p-4 text-center">
+              <Transition.Child
+                as={Fragment}
+                enter="ease-out duration-300"
+                enterFrom="opacity-0 scale-95"
+                enterTo="opacity-100 scale-100"
+                leave="ease-in duration-200"
+                leaveFrom="opacity-100 scale-100"
+                leaveTo="opacity-0 scale-95"
+              >
+                <Dialog.Panel className="w-full max-w-md transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all">
+                  <Dialog.Title
+                    as="h3"
+                    className="text-lg font-medium leading-6 text-gray-900"
+                  >
+                  
+                  </Dialog.Title>
+                  <div className="mt-2">
+                    <img src="assets/img/tick-grey.png" />
+                      <h5>Thank you for Your Registration .</h5>
+                      <h5>
+                        Please wait until your profile verification is completed
+                        .
+                      </h5>
+
+                  </div>
+
+                  <div className="mt-4">
+                    <button
+                      type="button"
+                      id="CloseBidModel"
+                      className="inline-flex justify-center rounded-md border border-transparent bg-blue-100 px-4 py-2 text-sm font-medium text-blue-900 hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2 hidden"
+                      onClick={closeModal}
+                    >
+                      Got it, thanks!
+                    </button>
+                    <button
+                          onClick={() => {
+                            history("/");
+                            document.getElementById("CloseBidModel").click();
+                          }}
+                          type="submit"
+                          class="btn dark-btn width-set mx-auto"
+                        >
+                          Ok
+                        </button>
+                  </div>
+                </Dialog.Panel>
+              </Transition.Child>
+            </div>
+          </div>
+        </Dialog>
+      </Transition>
         
 
        
