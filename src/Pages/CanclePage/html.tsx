290ad497406ea55fab5335107bd74da313e58586
deepak <deepak@yopmail.com>
2024-09-30 18:32:28 +0530

    chnages for the designs and images

--- src/Pages/CanclePage/html.tsx
+++ src/Pages/CanclePage/html.tsx
@@ -1,3 +1,5 @@
+import { AiOutlineCloseCircle } from "react-icons/ai";
+import { SlCheck } from "react-icons/sl";
 
 const Html = ({
 
@@ -5,7 +7,21 @@ const Html = ({
 
   return (
     <>
-    Cancle Page
+     <div className="h-screen flex items-center justify-center bgimgssuc">
+        <div className="bg-white shadow-lg rounded-lg p-8 max-w-md text-center">
+            <div className="mb-6">
+            <AiOutlineCloseCircle className="w-16 h-16 mx-auto text-red-500 "  />
+
+            
+            </div>
+            <h1 className="text-3xl font-bold text-gray-800 mb-4">Thank You!</h1>
+            <p className="text-gray-600 mb-6">Your submission has been received. We will get back to you shortly.</p>
+
+            <a href="/" className="inline-block bg-red-500 hover:bg-green-600 text-white font-semibold py-2 px-4 rounded transition duration-300 ease-in-out">
+              Go back to Home
+            </a>
+          </div>
+        </div>
    
      
     </>
