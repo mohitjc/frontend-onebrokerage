d6f43e5d81099ffda1c4ff04d2425aa9070ffd99
upakshi <upakshi@jcsoftwaresolution.com>
2024-10-17 10:35:21 +0530

    changes

--- src/environment/index.js
+++ src/environment/index.js
@@ -1,10 +1,7 @@
 const environment = {
-  // api: 'http://195.35.8.196:6084/',
-  api: 'https://4193-2404-7c80-c-1609-d161-782a-b988-6598.ngrok-free.app/',
+  api: 'http://195.35.8.196:6084/',
   googleAPIKey: "AIzaSyDhsAnr0k0oWjWn48iqiW4XUX4dvWzvtMA",
-  // chat_api: 'http://195.35.8.196:9084/',
-  chat_api:'https://rnnfp-2404-7c80-c-1609-d161-782a-b988-6598.a.free.pinggy.link/',
-
+  chat_api: 'http://195.35.8.196:9084/',
   appId:'cc0be22ca9904c11b616136fe8aa34a0',
   map_api_key: '',
   userRoleId: 'user',
