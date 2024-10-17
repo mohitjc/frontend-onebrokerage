3c7d99b8c265fb2671e9b16e19ba5faf6ec6d851
upakshi <upakshi@jcsoftwaresolution.com>
2024-10-17 10:39:11 +0530

    destination country not shwoing

--- src/Pages/TaskManagement/View.jsx
+++ src/Pages/TaskManagement/View.jsx
@@ -242,7 +242,7 @@ const View = () => {
                                             {data && data?.origin_location_postal_code}
                                         </p>
                                     </div>
-                                    <div className="col-span-12 lg:col-span-6 flex flex-col  ">
+                                    {data?.origin_location_country?   <div className="col-span-12 lg:col-span-6 flex flex-col  ">
                                         <label className="text-[14px] text-[#0000009c] tracking-wider  mb-1">
                                             Origin Country
                                         </label>
@@ -250,7 +250,8 @@ const View = () => {
                                             {/* <MdOutlineEmail className="text-xl text-[#494f9f]" /> */}
                                             {data && data?.origin_location_country}
                                         </p>
-                                    </div>
+                                    </div>:<></>}
+                                 
                                 </div>
                                 <div className="grid grid-cols-12 p-4 bg-[#f9faff] m-4 border border-[#474e9c0f]">
                                     <div className="col-span-12  mb-7">
@@ -293,7 +294,7 @@ const View = () => {
                                             {data && data?.destination_location_postal_code}
                                         </p>
                                     </div>
-                                    <div className="col-span-12 lg:col-span-6 flex flex-col  ">
+                                    {data?.destination_location_country? <div className="col-span-12 lg:col-span-6 flex flex-col  ">
                                         <label className="text-[14px] text-[#0000009c] tracking-wider  mb-1">
                                             Destination Country
                                         </label>
@@ -301,7 +302,8 @@ const View = () => {
                                             {/* <MdOutlineEmail className="text-xl text-[#494f9f]" /> */}
                                             {data && data?.destination_location_country}
                                         </p>
-                                    </div>
+                                    </div>:<></>}
+                                   
                                 </div>
                             </div>
                         </div>
