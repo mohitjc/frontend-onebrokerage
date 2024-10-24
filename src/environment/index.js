d28fb784c2dcecbcd58341ff229c959b7b73f05a
upakshi <upakshi@jcsoftwaresolution.com>
2024-10-24 11:32:28 +0530

    change api key for agora

--- src/environment/index.js
+++ src/environment/index.js
@@ -2,7 +2,7 @@ const environment = {
   api: 'https://obapi.jcsoftwaresolution.in/',
   googleAPIKey: "AIzaSyDhsAnr0k0oWjWn48iqiW4XUX4dvWzvtMA",
   chat_api: 'https://obchat.jcsoftwaresolution.in/',
-  appId:'91028c3f620b484c98bfd5322010ac40',
+  appId:'cc0be22ca9904c11b616136fe8aa34a0',
   map_api_key: '',
   userRoleId: 'user',
   adminRoleId: 'admin',
